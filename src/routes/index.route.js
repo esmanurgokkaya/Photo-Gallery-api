9ae9a3f353c60a64539a36a7b9acd4ce798de1ca
Esma <esmanurgkky10@gmail.com>
2025-12-03 17:14:23 +0300

    feat: add photo upload endpoint with multer validation

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import AlbumRouter from './album.route.js';
+import PhotoRouter from './photo.route.js';
 const router = express.Router();
 
 router.use("/albums", AlbumRouter);
+router.use("/photos", PhotoRouter);
 
 export default router;
