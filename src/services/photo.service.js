046a0432a5dcb769f123a47f327223bfd615e1b3
Esma <esmanurgkky10@gmail.com>
2025-12-05 22:34:42 +0300

    feat: enhance uploadPhotoToS3 method to include image metadata in response

--- src/services/photo.service.js
+++ src/services/photo.service.js
@@ -1,9 +1,13 @@
 import photoModel from "../models/photo.model.js";
 import s3Model from "../models/s3.model.js";
 import env from "../utils/env.js";
+import sharp from "sharp";
 
 class PhotoService {
     async uploadPhotoToS3(file) {
+        const image = sharp(file.buffer);
+        const metaData = await image.metadata();
+
         const params = {
             Bucket: env.bucketName,
             Key: file.originalname,
@@ -11,10 +15,14 @@ class PhotoService {
             ContentType: file.mimetype,
         };
         const s3Result = await s3Model.uploadImage(params);        
-        // Controller'a geri döndürülecek URL'yi burada oluştur
         const photoUrl = `https://${env.bucketName}.s3.${env.bucketRegion}.amazonaws.com/${file.originalname}`;
         
-        return { photoUrl, s3Result }; 
+        return { photoUrl, s3Result, 
+            metadata: {
+                dimensions: `${metaData.width}x${metaData.height}`,
+                size: file.size,
+                upload_date: new Date(),
+            } }; 
     
 
     }
